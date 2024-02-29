992271d68665109560b28b37d18879f99fc70519
MatLeg <mat.legierskii@gmail.com>
2024-02-29 20:57:34 +0100

    add userBox, init implementation

--- app/src/main/java/com/example/offlinenosql_db/MainActivity.kt
+++ app/src/main/java/com/example/offlinenosql_db/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.offlinenosql_db.data.local.entities.User
 import com.example.offlinenosql_db.ui.theme.OfflineNoSQLDBTheme
+import com.example.offlinenosql_db.utils.ObjectBox
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+
+                    val userBox = ObjectBox.store.boxFor(User::class.java)
+                    val user = User(name = "Tina")
+                    userBox.put(user)
                 }
             }
         }
