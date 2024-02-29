1732ce71ed1de81ea04eef2d11ad53aa9ec9fc3d
MatLeg <mat.legierskii@gmail.com>
2024-02-29 19:11:11 +0100

    add objectbox db

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,17 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+buildscript {
+    val objectboxVersion by extra("3.8.0") // For KTS build scripts
+
+    repositories {
+        mavenCentral() // Note: 2.9.0 and older are available on jcenter()
+    }
+
+    dependencies {
+        // Android Gradle Plugin 4.1.0 or later supported.
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("io.objectbox:objectbox-gradle-plugin:$objectboxVersion")
+    }
+}
